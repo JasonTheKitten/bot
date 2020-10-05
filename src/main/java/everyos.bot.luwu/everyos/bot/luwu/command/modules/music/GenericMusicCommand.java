e6249245ea0c9e85210ec9aff953e310676ac93f
JasonTheKitten <jason05gronn@gmail.com>
2020-10-05 09:43:05 -0400

    Git seems to have rolled back my files

--- src/main/java/everyos.bot.luwu/everyos/bot/luwu/command/modules/music/GenericMusicCommand.java
+++ src/main/java/everyos.bot.luwu/everyos/bot/luwu/command/modules/music/GenericMusicCommand.java
@@ -2,7 +2,6 @@ package everyos.bot.luwu.command.modules.music;
 
 import everyos.bot.luwu.command.Command;
 import everyos.bot.luwu.command.CommandData;
-import everyos.bot.luwu.command.modules.music.MusicAdapter.VoiceStateMissingException;
 import everyos.bot.luwu.entity.Channel;
 import everyos.bot.luwu.entity.Message;
 import everyos.bot.luwu.parser.ArgumentParser;
@@ -12,7 +11,7 @@ public abstract class GenericMusicCommand implements Command {
 	@Override public Mono<?> execute(CommandData data, ArgumentParser parser) {
 		Channel channel = data.getChannel();
 		Message message = data.getMessage();
-		return message.suppressEmbeds(true)
+		/*return message.suppressEmbeds(true)
 			.onErrorResume(e->Mono.empty())
 			.then(message.getAuthorAsMember())
         	.flatMap(m->MusicAdapter.getFromMember(data.getBotInstance(), m))
@@ -23,11 +22,13 @@ public abstract class GenericMusicCommand implements Command {
         			return channel.createMessage(data.getLocale().localize("command.music.notinchannel"));
         		}
         		return Mono.error(e);
-        	});
+        	});*/
 		//TODO: We should scan out VoiceStates in other guilds if the current guild does not have a voice state
+		
+		return Mono.empty();
 	}
 
-	abstract Mono<?> execute(Message message, CommandData data, String argument, MusicAdapter ma, MessageChannel channel);
+	//abstract Mono<?> execute(Message message, CommandData data, String argument, MusicAdapter ma, MessageChannel channel);
 
 	public boolean requiresDJ() {return true;}
 }
